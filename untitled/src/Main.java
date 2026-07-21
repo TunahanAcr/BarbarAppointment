ebb1b5ecec5419a279872e4cda2cd79f3109ef8f
TunahanAcr <acartunahan55@gmail.com>
2026-07-21 14:01:47 +0300

    chore: gitignore kuralları uygulandı ve gereksiz dosyalar temizlendi

--- untitled/src/Main.java
+++
@@ -1,9 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    Scanner input = new Scanner(System.in);
-    String myInput = input.nextLine();
-
-    String[] split = myInput.trim().split("\\s+");
-    System.out.println(split.length);
-}
